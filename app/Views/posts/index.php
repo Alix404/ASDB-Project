73d68408e0e101c7ea2fe4d275c8ba3e67274f4d
Alix Vendeville <vendalex01@gmail.com>
2020-05-19 11:03:32 +0200

    Graphic overhaul changed

--- app/Views/posts/index.php
+++ app/Views/posts/index.php
@@ -1,9 +1,8 @@
 <div class="row">
-    <div class="col-sm-8" id="postsCard">
-        <h1 class="card-header my-4">Les articles</h1>
+    <div class="col-sm-12 col-lg-8 postsCard">
         <?php
         if (!empty($articles)): ?>
-            <div class="card mt-4 mb-4">
+            <div class="card">
                 <div class="card-body">
                     <?php
 
@@ -16,28 +15,28 @@
                 </div>
             </div>
         <?php else: ?>
-            <div class="card mt-4 mb-4">
+            <div class="card">
+                <h1 class="card-header">Les articles</h1>
                 <div class="card-body">
-                    <h5>Il n'y a malheureusement aucuns articles dans cette catégorie, elle sera soit supprimée, soit
-                        réalimenté ultérieurement<br><br><a href="index.php?p=posts.index">revenir à l'Accueil</a></h5>
+                    <p>Il n'y a malheureusement aucuns articles dans cette catégorie, elle sera soit supprimée, soit
+                        réalimenté ultérieurement</p><br><br><a href="index.php?p=posts.index">revenir à l'Accueil</a>
                 </div>
             </div>
         <?php endif; ?>
     </div>
-</div>
 
-<div class="col-sm-4" id="categoryCard">
-    <div class="card my-4">
-        <h5 class="card-header">Les catégories</h5>
-        <div class="card-body">
-            <ul class="list-unstyled mb-0">
-                <?php foreach ($categories as $categorie): ?>
-                    <li>
-                        <a href="<?= $categorie->url; ?>"><?= $categorie->titre; ?></a>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
+    <div class="col-sm-12 col-lg-4 categoryCard">
+        <div class="card">
+            <h5 class="card-header">Les catégories</h5>
+            <div class="card-body">
+                <ul class="list-unstyled mb-0">
+                    <?php foreach ($categories as $categorie): ?>
+                        <li>
+                            <a href="<?= $categorie->url; ?>"><?= $categorie->titre; ?></a>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
         </div>
     </div>
-</div>
 </div>
--- app/Views/posts/show.php
+++ app/Views/posts/show.php
@@ -1,11 +1,12 @@
-<div class="row" id="posts">
-    <div class="col-sm-8" id="postsCard">
-        <h1 class="card-header my-4"><?= $articles->titre; ?></h1>
-        <div class="card mb-4">
+<div class="row">
+    <div class="col-sm-12 col-lg-8 postsCard">
+        <h1 class="card-header"><?= $articles->titre; ?></h1>
+        <div class="card">
             <div class="card-body">
                 <p><em><?= $articles->categorie; ?></em></p>
 
                 <p><?= $articles->contenu; ?></p>
+                <a href="index.php?p=posts.index">revenir à l'Accueil</a>
             </div>
         </div>
     </div>
